bd71e46df949f3e9abb76ff84c117cea0843e716
Nakibul Hassan <nakibhasan2711@gmail.com>
2025-05-03 18:08:03 +0600

    - added complete dependency

--- app/src/main/java/com/nakibul/android/boilerplateproject/MainActivity.kt
+++ app/src/main/java/com/nakibul/android/boilerplateproject/MainActivity.kt
@@ -4,44 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nakibul.android.boilerplateproject.ui.theme.BoilerplateProjectTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BoilerplateProjectTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-git add
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BoilerplateProjectTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/nakibul/android/boilerplateproject/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.nakibul.android.boilerplateproject
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication:Application() {
+}
